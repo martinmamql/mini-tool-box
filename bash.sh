660ef5034b060b276d42a1e148e978fbd0734b6b
Martin Ma <martin.q.ma0@gmail.com>
2022-02-24 10:36:40 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # zip all folders into individual zip files
 for i in */; do zip -r "${i%/}.zip" "$i"; done
+
+# Download Google drive files
+# https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
