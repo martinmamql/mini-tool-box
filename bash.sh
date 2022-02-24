189840418641f2e45a072f96730f3e29ac5588eb
Martin Ma <martin.q.ma0@gmail.com>
2022-02-24 14:22:58 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,3 +5,7 @@ for i in */; do zip -r "${i%/}.zip" "$i"; done
 # Download Google drive files
 # https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
 # https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
+
+# Replace strings in all files in a folder:
+# https://linuxize.com/post/how-to-find-files-in-linux-using-the-command-line/
+find . -type f -exec sed -i 's/\/work\/awilf/\/work\/qianlim\/mtag/g' {} +
