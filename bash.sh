a2d79425036f73bcac578c5465f4363ea4f9b98f
Martin Ma <martin.q.ma0@gmail.com>
2022-02-24 10:43:29 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,3 +4,4 @@ for i in */; do zip -r "${i%/}.zip" "$i"; done
 
 # Download Google drive files
 # https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
+# https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
