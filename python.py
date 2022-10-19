4ef2b579571567074f555079cad5a0ad0311c918
Martin Ma <martin.q.ma0@gmail.com>
2022-10-19 09:56:59 -0700

    Update python.py

--- python.py
+++ python.py
@@ -10,3 +10,16 @@ import fusion as fusion
 # Correction:
 from . import fusion
 from . import modules
+
+# Set two arguments referring to true and false of the same value
+parser.add_argument('--global_pool', action='store_true')
+parser.set_defaults(global_pool=False)
+parser.add_argument('--cls_token', action='store_false', dest='global_pool',
+                    help='Use class token instead of global pool for classification')
+
+# in bash
+AGGRE_TYPE="cls_token"
+#AGGRE_TYPE="global_pool"
+
+python ... 
+--$AGGRE_TYPE
